d2464de877560545ee37f666e6b3287e47c10b4f
ChamiduDev-5224 <pplchamiduravihara@gmail.com>
2024-10-31 19:56:27 +0530

    user entity added

---
+++ todoApp/src/main/java/com/example/todoApp/model/User.java
@@ -0,0 +1,57 @@
+package com.example.todoApp.model;
+
+import jakarta.persistence.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Entity
+@Table(name = "User")
+public class User implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "Id")
+    Integer id;
+
+    @Column(name = "Email")
+    String email;
+
+    @Column(name = "Password")
+    String password;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return "";
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
