77d29e4ba472df5d1f35eb71d151bc91983d38bf
ChamiduDev-5224 <pplchamiduravihara@gmail.com>
2024-11-06 07:55:01 +0530

    repo query utilized

--- todoApp/src/main/java/com/example/todoApp/repository/UserRepository.java
+++ todoApp/src/main/java/com/example/todoApp/repository/UserRepository.java
@@ -2,9 +2,11 @@ package com.example.todoApp.repository;
 
 import com.example.todoApp.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
-
+    @Query(value = "SELECT * FROM User WHERE id=?1", nativeQuery=true)
+    User getUserId(int id);
 }
